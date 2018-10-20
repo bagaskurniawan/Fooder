3d4bc8047a4c856e30a0dbe803fab821b1962f82
unknown <krnwnbgs@gmail.com>
2018-10-20 18:24:00 +0700

    Frontend home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,248 @@
+@extends('master')
+
+@section('content')
+<section class="engine"><a href="#"></a></section><section class="carousel slide cid-r6fHlgWGVt" data-interval="false" id="slider1-l">
+
+    
+
+    <div class="full-screen"><div class="mbr-slider slide carousel" data-pause="true" data-keyboard="false" data-ride="carousel" data-interval="4000"><ol class="carousel-indicators"><li data-app-prevent-settings="" data-target="#slider1-l" class=" active" data-slide-to="0"></li><li data-app-prevent-settings="" data-target="#slider1-l" data-slide-to="1"></li><li data-app-prevent-settings="" data-target="#slider1-l" data-slide-to="2"></li></ol><div class="carousel-inner" role="listbox"><div class="carousel-item slider-fullscreen-image active" data-bg-video-slide="false" style="background-image: url(assets/images/3-1920x837.jpg);"><div class="container container-slide"><div class="image_wrapper"><div class="mbr-overlay" style="background-color: rgb(0, 0, 0);"></div><img src="assets/images/3-1920x837.jpg"><div class="carousel-caption justify-content-center"><div class="col-10 align-center"><h2 class="mbr-fonts-style display-2">FULL SCREEN SLIDER</h2><p class="lead mbr-text mbr-fonts-style display-5">Choose from the large selection of latest pre-made blocks - jumbotrons, hero images, parallax scrolling, video backgrounds, hamburger menu, sticky header and more.</p><div class="mbr-section-btn" buttons="0"><a class="btn btn-success display-4" href="#m">CHECK IT OUT</a> </div></div></div></div></div></div><div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets/images/bread-breakfast-coffee-374052-2000x1333.jpg);"><div class="container container-slide"><div class="image_wrapper"><div class="mbr-overlay"></div><img src="assets/images/bread-breakfast-coffee-374052-2000x1333.jpg"><div class="carousel-caption justify-content-center"><div class="col-10 align-left"><h2 class="mbr-fonts-style display-2">VIDEO SLIDE</h2><p class="lead mbr-text mbr-fonts-style display-5">Slide with youtube video background and color overlay. Title and text are aligned to the left.</p></div></div></div></div></div><div class="carousel-item slider-fullscreen-image" data-bg-video-slide="false" style="background-image: url(assets/images/5-1-1920x837.jpg);"><div class="container container-slide"><div class="image_wrapper"><div class="mbr-overlay"></div><img src="assets/images/5-1-1920x837.jpg"><div class="carousel-caption justify-content-center"><div class="col-10 align-right"><h2 class="mbr-fonts-style display-2">IMAGE SLIDE</h2><p class="lead mbr-text mbr-fonts-style display-5">Choose from the large selection of latest pre-made blocks - jumbotrons, hero images, parallax scrolling, video backgrounds, hamburger menu, sticky header and more.</p><div class="mbr-section-btn" buttons="0"><a class="btn display-4 btn-primary" href="#m">CHECK IT OUT</a> </div></div></div></div></div></div></div><a data-app-prevent-settings="" class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#slider1-l"><span aria-hidden="true" class="mbri-left mbr-iconfont"></span><span class="sr-only">Previous</span></a><a data-app-prevent-settings="" class="carousel-control carousel-control-next" role="button" data-slide="next" href="#slider1-l"><span aria-hidden="true" class="mbri-right mbr-iconfont"></span><span class="sr-only">Next</span></a></div></div>
+
+</section>
+
+
+<section id="page-breaker" class="features17 cid-r6IJtNmiRl" id="features17-z">
+    
+    <div class="container-fluid">
+       <h5>Food</h5>
+        <div class="media-container-row">
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/ayamgoreng-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore</p>
+                        <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/spageti-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore</p>
+                           <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/nasi-udang-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore
+                        </p>
+                        <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/sate-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore
+                        </p>
+                        <a href="">Chek it out...</a>
+                    </div>
+                    <div class="mbr-section-btn align-right py-4"><a class="btn btn-primary display-4" href="#">View all</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="page-breaker" class="features17 cid-r6IMG72CsD" id="features17-13">
+    
+    <div class="container-fluid">
+      <h5>Promo</h5>
+        <div class="media-container-row">
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/promo7-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore</p>
+                           <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/promo10-1-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore
+                       </p>
+                       <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/promo2-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore
+                        </p>
+                        <a href="">Chek it out...</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card p-3 col-12 col-md-6 col-lg-3">
+                <div class="card-wrapper">
+                    <div class="card-img">
+                        <img src="assets/images/promo1-262x175.jpg" alt=“Fooder” title="">
+                    </div>
+                    <div class="card-box">
+                        <h4 class="card-title pb-3 mbr-fonts-style display-7">
+                            Lorem Ipsum
+                        </h4>
+                        <p class="mbr-text mbr-fonts-style display-7">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                           tempor incididunt ut labore et dolore
+                        </p>
+                        <a href="">Chek it out...</a>
+                    </div>
+                    <div class="mbr-section-btn align-right py-4"><a class="btn btn-primary display-4" href="#">View all</a></div>
+                </div>
+            </div>
+            
+            
+        </div>
+    </div>
+</section>
+
+<section id="page-breaker" class="mbr-section content4 cid-r6Ils24bNh" id="content4-u">
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-2">
+                    Article Title with Solid Background
+                </h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section content6 cid-r6IltPHG1y" id="content6-v">
+    
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-12">
+                <div class="media-container-row">
+                    <div class="mbr-figure" style="width: 60%;">
+                      <img src="assets/images/article1-616x385.jpg" alt=“Fooder” title="">  
+                    </div>
+                    <div class="media-content">
+                        <div class="mbr-section-text">
+                            <p>
+                               <h4> Lorem ipsum </h4>
+                               Lorem Ipsumdolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <div class="mbr-section-btn align-left py-4"><a class="btn btn-primary display-4" href="#">Read more</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section content7 cid-r6Ilu7eRxL" id="content7-w">
+          
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="col-12">
+                <div class="media-container-row">
+                    <div class="media-content">
+                        <div class="mbr-section-text">
+                            <p>
+                               <h4> Lorem ipsum </h4>
+                               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <div class="mbr-section-btn align-right py-4"><a class="btn btn-primary display-4" href="#">Read more</a></div>
+                        </div>
+                    </div>
+                    <div class="mbr-figure" style="width: 60%;">
+                     <img src="assets/images/article2-650x390.jpg" alt=“Fooder” title="">  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.8.4, #m -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.8.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/003-restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle-122x122.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-0">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="#top">
+                         <img src="assets/images/003-restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle-122x122.png" alt=“Fooder” title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-5" href="#m">FOODER</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black display-4" href="#m">Home</a></li><li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="#m">Promo</a>
+                </li><li class="nav-item"><a class="nav-link link text-black display-4" href="#m">Food</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="#m">Article</a></li>
+                <li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="#m">
+                        
+                        About Us
+                    </a>
+                </li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-black display-4" href="#m"><span class="mbri-login mbr-iconfont mbr-iconfont-btn"></span>
+                    Login/Register</a></div>
+        </div>
+    </nav>
+</section>
+
+
+@yield('content')
+
+
+
+
+<section id="page-breaker" class="footer3 cid-r6FVw1VhnO" id="footer3-m">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content">
+            <div class="col-md-2 col-sm-4">
+                <div class="mb-3 img-logo">
+                    <a href="#">
+                         <img src="assets/images/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle-1-192x192.png" alt=“Fooder” title="">
+                    </a>
+                </div>
+                <p class="mbr-fonts-style foot-logo display-7">FOODER INC.</p>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <p class="mb-4 mbr-fonts-style foot-title display-7">
+                    RECENT NEWS
+                </p>
+                <p class="mbr-text mbr-links-column mbr-fonts-style display-7">Food&nbsp;<br>Promo&nbsp;<br>Article&nbsp;<br>About us&nbsp;<br><br></p>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <p class="mb-4 mbr-fonts-style foot-title display-7">
+                    CATEGORIES
+                </p>
+                <p class="mbr-text mbr-fonts-style mbr-links-column display-7">
+                    <a href="#" class="text-white">Business</a>
+                    <br><a href="#" class="text-white">Design</a>
+                    <br><a href="#" class="text-white">Real life</a>
+                    <br><a href="#" class="text-white">Science</a>
+                    <br><br></p>
+            </div>
+            <div class="col-md-4 col-sm-12">
+                <p class="mb-4 mbr-fonts-style foot-title display-7">
+                    SUBSCRIBE
+                </p>
+                <p class="mbr-text form-text mbr-fonts-style display-7">
+                    Get monthly updates and free resources.
+                </p>
+                <div class="media-container-column" data-form-type="formoid">
+                    <div data-form-alert="" hidden="" class="align-center">
+                        Thanks for filling out the form!
+                    </div>
+
+                    <form class="form-inline" action="#" method="post" data-form-title="Fooder Form">
+                        <input type="hidden" value="X9mq/k3T7wAgGfDcC5SaaaD/a8ZIuE2lSTQkPF9p0ZwYFki+kHd7MIjSfPKfhN+7YlCL6eD4YGx4QLQDzUn5P/t4S9rCf1jWuK33L96MtLRP8Z6TLhE6OsdDv/vl/t3c" data-form-email="true">
+                        <div class="form-group">
+                            <input type="email" class="form-control input-sm input-inverse my-2" name="email" required="" data-form-field="Email" placeholder="Email" id="email-footer3-m">
+                        </div>
+                        <div class="input-group-btn m-2">
+                            <button href="" class="btn btn-primary display-4" type="submit" role="button">Subscribe</button>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2018 Fooder Inc. - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon" style="color: rgb(250, 190, 34);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon" style="color: rgb(250, 190, 34);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.youtube.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon" style="color: rgb(250, 190, 34);"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://instagram.com/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon" style="color: rgb(250, 190, 34);"></span>
+                            </a>
+                        </div>
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/ytplayer/jquery.mb.ytplayer.min.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+  <script src="assets/formoid/formoid.min.js"></script>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i></i></a></div>
+  </body>
+</html>
+
