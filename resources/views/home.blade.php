1d13c279abfe0333a050b3a553156bd038345e7d
Joseph Michael P <michaeldorks@gmail.com>
2019-01-11 16:06:43 +0700

    carousel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -294,4 +294,38 @@
          <div id="button-article" class="mbr-section-btn align-center py-4"><a class="btn btn-primary display-4" href="/article">View more</a></div>
     </div>
 </section>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="ayamgoreng-262x175.jpg" alt="Ayam Goreng">
+        </div>
+
+        <div class="item">
+            <img src="spageti-262x175.jpg" alt="Spaghetti">
+        </div>
+
+        <div class="item">
+            <img src="nasi-udang-262x175.jpg" alt="Nasi Udang">
+        </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,39 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="ayamgoreng-262x175.jpg" alt="Ayam Goreng">
+                            </div>
+
+                            <div class="item">
+                                <img src="spageti-262x175.jpg" alt="Spaghetti">
+                            </div>
+
+                            <div class="item">
+                                <img src="nasi-udang-262x175.jpg" alt="Nasi Udang">
+                            </div>
+                        </div>
+
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
                 </div>
 
                 <div class="links">
